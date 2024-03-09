e70c4c39996ff712d8daf8d8f3624dbb2920f7f5
Mohitkothiyal01 <129210465+Mohitkothiyal01@users.noreply.github.com>
2024-03-09 19:18:47 +0000

    Done4Today

---
+++ Learn-Javascript/02-variables.js
@@ -0,0 +1,13 @@
+const AccountId = 1432
+
+var AccountholderName = "Mohit Kothiyal" // Do not use var bcuz of block scope problem.
+
+let AccountCity = "Rishikesh"
+
+// let AccountId = 1234  "NOT allowed beacause AccountId is constant.Cant change."
+
+//  AccountCity ="Dehradun" 
+
+
+console.table([AccountId,AccountholderName,AccountCity])
+//console.table is used to represent data in tabular form
