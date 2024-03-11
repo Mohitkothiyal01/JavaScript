93156cc68974bb8255812fe555ce4dd64e0af61c
Mohitkothiyal01 <129210465+Mohitkothiyal01@users.noreply.github.com>
2024-03-11 19:23:17 +0000

    Done

---
+++ Learn-Javascript/03-Datatypes.js
@@ -0,0 +1,20 @@
+let Name = "Aman" //String datatype.
+
+let Age = 25 //Number datatype.
+
+let TodayIsMonday = true //Boolean datatype.
+
+let Tank = null   //Null Datatype.
+
+let tempreture = undefined  //undefined datatype.
+
+console.log(typeof Tank);
+
+/* There are 7 primitive Datatypes in JS these are below
+1. String
+2. Number
+3. Boolean 
+4. NUll
+5. Undefined
+6. Symbol
+7. BigInt   used for big integer..   */
